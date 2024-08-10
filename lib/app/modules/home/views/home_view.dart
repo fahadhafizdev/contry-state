c0889db1e80bc7b116cdecaad2d3dfb8a873972b
Fahad Hafizudin Dayana <89493279+fahadhafizdev@users.noreply.github.com>
2024-08-10 12:43:15 +0700

    update

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -10,6 +10,7 @@ class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    HomeController c = Get.put(HomeController());
     return Scaffold(
         appBar: AppBar(
           title: Text(
